cff00fa2ea399a598963d77dae7d37c9464dd6d7
andre <andremp1927@hotmail.com>
2020-05-09 13:47:47 -0300

    cv, bg e outros

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component} from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-header',
@@ -12,17 +11,7 @@ export class HeaderComponent {
 
 
   constructor(
-    private snackBar: MatSnackBar,
 
   ){}
 
-  
-  CurriculumAlert(){
-    this.snackBar.open("Em Breve", "",{ duration: 5000 });
-  }
-
-  topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
 }
